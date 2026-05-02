19392390011f35e5c6210178ea57f5b1d2725631
Benjy <108727379+Benjy0812@users.noreply.github.com>
2026-05-02 02:12:12 +0200

    fix(config): comment out base path in vite.config.js for deployment flexibility

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // Uncomment the line below if you want to deploy to GitHub Pages or a subdirectory
-  base: '/WebCalculator/',
+  //base: '/WebCalculator/',
 })
