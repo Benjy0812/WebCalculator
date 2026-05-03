78b88854b6ca69d910d6e99f2ec02711f1878fd9
Benjy <108727379+Benjy0812@users.noreply.github.com>
2026-05-03 23:23:07 +0200

    BROKEN feat(calculator): integrate mathjs for enhanced calculation functionality

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+import { evaluate } from 'mathjs'
+
 const display = document.getElementById('display')
+const buttons = document.querySelectorAll('button')
 
 window.onload = function () {
   display.value = '0'
 }
 
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    console.log(button.textContent)
+  })
+})
+
 function appendValue(value) {
   if (
+    display.value === '0' ||
     display.value === 'Error' ||
     display.value === 'undefined' ||
     display.value === 'NaN'
